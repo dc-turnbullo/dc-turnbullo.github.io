304e503826d36037af1b840165e27f0be3d782b6
dc-turnbullo <turnbullo@dulwich.org.uk>
2024-11-07 19:23:56 +0000

    Update recursivefib.py

--- pythonotherthings/recursivefib.py
+++ pythonotherthings/recursivefib.py
@@ -1,5 +1,5 @@
 import time
-
+'''
 def fibonacci(n): 
 
     if n == 0:
@@ -7,9 +7,16 @@ def fibonacci(n):
     if n == 1:
         return 1 
     return fibonacci(n-1) + fibonacci(n-2) 
-	
+'''
+
+def fibonacci2(n): 
+    fibNumbers = [0,1]   
+    for i in range(2, n+1): 
+        fibNumbers.append(fibNumbers[i-1] + fibNumbers[i-2]) 
+        return fibNumbers[n]  
+
 start = time.time()
-fibonacci(100)
+num = fibonacci2(10)
 end = time.time()
 print(end-start)
 
